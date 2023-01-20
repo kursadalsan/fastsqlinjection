13d1d6a35d601d0cadde5284dd862572cded96da
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2023-01-21 02:00:59 +0300

    Update sql.sh

--- sql.sh
+++ sql.sh
@@ -2,4 +2,4 @@
 
 mkdir $1 
 cd $1
-findomain -t $1 -q | httpx -silent -mc 200 | anew | getallurls | grep $1 | gf sqli >> sqli ; sqlmap -m sqli --batch --random-agent --level 1
+findomain -t $1 -q | httpx-toolkit -silent -mc 200 | anew | getallurls | grep $1 | gf sqli >> sqli ; sqlmap -m sqli --batch --random-agent --level 1
